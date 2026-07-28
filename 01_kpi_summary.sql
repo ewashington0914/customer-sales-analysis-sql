beb4513542ef6cba3e833aa26f03794ef92c85f0
Eric Washington <82563770+ewashington0914@users.noreply.github.com>
2026-07-27 19:26:10 -0500

    Delete 01_kpi_summary.sql

--- 01_kpi_summary.sql
+++
@@ -1,9 +0,0 @@
-/*
-KPIS
-*/
-SELECT
-    ROUND(SUM(o.units * p.unit_price), 2) AS total_revenue,
-    COUNT(DISTINCT o.customer_id) AS total_customers
-FROM orders o
-INNER JOIN products p
-    ON o.product_id = p.product_id;
